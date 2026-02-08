8305cec263826c101ab03d454ae3bd04ad74867e
Prince <prince1234360@gmail.com>
2026-02-08 22:28:27 +0530

    Enable CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.soil import router as soil_router
 
 app = FastAPI(title="AI Soil Health Analyzer")
+
+# ADD THIS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all for testing
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(soil_router)
